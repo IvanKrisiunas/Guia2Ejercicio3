fa6595bc05c2392b1723dc8a56d1a048009b919d
IvanKrisiunas <141675048+IvanKrisiunas@users.noreply.github.com>
2023-08-14 17:16:32 -0300

    Initial commit

---
+++ src/guia2ejercicio3/Guia2Ejercicio3.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package guia2ejercicio3;
+
+/**
+ *
+ * @author Monica
+ */
+public class Guia2Ejercicio3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Ejercicio3 e3 = new Ejercicio3();
+        e3.setVisible(true);
+        e3.setLocationRelativeTo(null);
+    }
+    
+}
